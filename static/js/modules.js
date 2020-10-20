cc1714e5593f56f0f2ee9294ae8d48028ab1b357
Kristian Møller Schmidt <34898026+KristianMSchmidt@users.noreply.github.com>
2020-10-20 12:55:38 +0200

    disabled solve button after solution

--- static/js/modules.js
+++ static/js/modules.js
@@ -96,7 +96,8 @@ function setup_custom_sudoku(sudoku){
 function show_succes(solved_sudoku, solution_method, solution_time){
     show_board(solved_sudoku, is_solution=true); 
     document.getElementById("solution_feedback").style.color = "green";
-    document.getElementById("solution_feedback").innerHTML = `Solved server-side in ${solution_time} seconds`
+    document.getElementById("solution_feedback").innerHTML = `Solved server-side in ${solution_time} seconds`;
+    document.getElementById("solve_btn").disabled = true;
 }
 
 function show_failure(){
